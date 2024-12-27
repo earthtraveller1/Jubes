75a5571604936a4a29423c08ce6fad45bde37450
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-12-27 17:02:44 -0500

    Formatted document + added some more getters.

--- src/present.hpp
+++ src/present.hpp
@@ -5,21 +5,27 @@
 
 class Swapchain {
   public:
-    Swapchain(const Device& device, GLFWwindow* window);
+    Swapchain(const Device &device, GLFWwindow *window);
 
     NO_COPY(Swapchain)
 
     inline VkFormat get_format() const { return image_format; }
 
+    inline const std::vector<VkImageView> &get_image_views() const {
+        return image_views;
+    }
+
+    inline const VkExtent2D& get_extent() const { return extent; }
+
     ~Swapchain();
 
-    private:
-        const Device& device;
+  private:
+    const Device &device;
 
-        VkSwapchainKHR swapchain;
-        VkFormat image_format;
-        VkExtent2D extent;
+    VkSwapchainKHR swapchain;
+    VkFormat image_format;
+    VkExtent2D extent;
 
-        std::vector<VkImage> images;
-        std::vector<VkImageView> image_views;
+    std::vector<VkImage> images;
+    std::vector<VkImageView> image_views;
 };
