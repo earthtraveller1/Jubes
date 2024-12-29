74f303508de53cea4174331d4709e999e781da16
Liangyue " Yue <liangyuezhao7@gmail.com>
2024-12-28 19:10:08 -0500

    Stole some code from my previous project for the graphics pipeline (lol).

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -84,3 +84,242 @@ Framebuffers::Framebuffers(const Device &p_device, const Swapchain &p_swapchain,
         framebuffers.push_back(fb);
     }
 }
+
+GraphicsPipeline::GraphicsPipeline(
+    const Device &p_device, const RenderPass &p_render_pass,
+    std::string_view p_vertex_shader_path,
+    std::string_view p_fragment_shader_path,
+    std::span<const VkPushConstantRange> push_constant_ranges,
+    std::span<const VkDescriptorSetLayout> p_descriptor_set_layouts)
+    : device(p_device), render_pass(p_render_pass) {
+    const VkPipelineLayoutCreateInfo pipeline_layout_create_info{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .setLayoutCount =
+            static_cast<uint32_t>(p_descriptor_set_layouts.size()),
+        .pSetLayouts = p_descriptor_set_layouts.data(),
+        .pushConstantRangeCount =
+            static_cast<uint32_t>(push_constant_ranges.size()),
+        .pPushConstantRanges = push_constant_ranges.data(),
+    };
+
+    auto result =
+        vkCreatePipelineLayout(p_device.get(), &pipeline_layout_create_info,
+                               nullptr, &layout);
+
+    if (result != VK_SUCCESS) {
+        fmt::println("[ERROR]: Failed to create the Vulkan pipeline layout: {}",
+                     result);
+        throw Error::VulkanError;
+    }
+
+    const auto vertex_shader_code = read_as_bytes(p_vertex_shader_path);
+    const auto fragment_shader_code = read_as_bytes(p_fragment_shader_path);
+
+    const VkShaderModuleCreateInfo vertex_shader_module_create_info{
+        .sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .codeSize = vertex_shader_code.size(),
+        .pCode = reinterpret_cast<const uint32_t *>(vertex_shader_code.data()),
+    };
+
+    VkShaderModule vertex_shader_module;
+    result =
+        vkCreateShaderModule(p_device.get(), &vertex_shader_module_create_info,
+                             nullptr, &vertex_shader_module);
+
+    if (result != VK_SUCCESS) {
+        fmt::println("[ERROR]: Failed to create the shader module for {}: {}",
+                     p_vertex_shader_path, result);
+        throw Error::VulkanError;
+    }
+
+    const VkShaderModuleCreateInfo fragment_shader_module_create_info{
+        .sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .codeSize = fragment_shader_code.size(),
+        .pCode =
+            reinterpret_cast<const uint32_t *>(fragment_shader_code.data()),
+    };
+
+    VkShaderModule fragment_shader_module;
+    result = vkCreateShaderModule(p_device.get(),
+                                  &fragment_shader_module_create_info, nullptr,
+                                  &fragment_shader_module);
+
+    if (result != VK_SUCCESS) {
+        fmt::println("[ERROR]: Failed to create the shader module for {}: {}",
+                     p_fragment_shader_path, result);
+        throw Error::VulkanError;
+    }
+
+    const std::array<VkPipelineShaderStageCreateInfo, 2> shader_stages{
+        VkPipelineShaderStageCreateInfo{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+            .pNext = nullptr,
+            .flags = 0,
+            .stage = VK_SHADER_STAGE_VERTEX_BIT,
+            .module = vertex_shader_module,
+            .pName = "main",
+            .pSpecializationInfo = nullptr},
+        VkPipelineShaderStageCreateInfo{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+            .pNext = nullptr,
+            .flags = 0,
+            .stage = VK_SHADER_STAGE_FRAGMENT_BIT,
+            .module = fragment_shader_module,
+            .pName = "main",
+            .pSpecializationInfo = nullptr,
+        },
+    };
+
+    const VkPipelineVertexInputStateCreateInfo vertex_input_state_create_info{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .vertexBindingDescriptionCount = 1,
+        .pVertexBindingDescriptions = &vertex_input_binding_description,
+        .vertexAttributeDescriptionCount = vertex_attribute_descriptions.size(),
+        .pVertexAttributeDescriptions = vertex_attribute_descriptions.data(),
+    };
+
+    const VkPipelineInputAssemblyStateCreateInfo input_assembly_state{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST,
+        .primitiveRestartEnable = VK_FALSE,
+    };
+
+    const VkPipelineViewportStateCreateInfo viewport_state{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .viewportCount = 1,
+        .pViewports = nullptr,
+        .scissorCount = 1,
+        .pScissors = nullptr};
+
+    const VkPipelineRasterizationStateCreateInfo rasterization_state{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .depthClampEnable = VK_FALSE,
+        .rasterizerDiscardEnable = VK_FALSE,
+        .polygonMode = VK_POLYGON_MODE_FILL,
+        .cullMode = VK_CULL_MODE_BACK_BIT,
+        .frontFace = VK_FRONT_FACE_CLOCKWISE,
+        .depthBiasEnable = VK_FALSE,
+        .depthBiasConstantFactor = 0.0f,
+        .depthBiasClamp = 0.0f,
+        .depthBiasSlopeFactor = 0.0f,
+        .lineWidth = 1.0f,
+    };
+
+    const VkPipelineMultisampleStateCreateInfo multisample_state{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .rasterizationSamples = VK_SAMPLE_COUNT_1_BIT,
+        .sampleShadingEnable = VK_FALSE,
+        .minSampleShading = 0.0f,
+        .pSampleMask = nullptr,
+        .alphaToCoverageEnable = VK_FALSE,
+        .alphaToOneEnable = VK_FALSE,
+    };
+
+    const VkPipelineDepthStencilStateCreateInfo depth_stencil_state{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .depthTestEnable = VK_TRUE,
+        .depthWriteEnable = VK_TRUE,
+        .depthCompareOp = VK_COMPARE_OP_LESS,
+        .depthBoundsTestEnable = VK_FALSE,
+        .stencilTestEnable = VK_FALSE,
+        .front = {},
+        .back = {},
+        .minDepthBounds = 0.0f,
+        .maxDepthBounds = 0.0f,
+    };
+
+    const VkPipelineColorBlendAttachmentState color_blend_attachment{
+        .blendEnable = VK_FALSE,
+        .srcColorBlendFactor = VK_BLEND_FACTOR_ZERO,
+        .dstColorBlendFactor = VK_BLEND_FACTOR_ZERO,
+        .colorBlendOp = VK_BLEND_OP_ADD,
+        .srcAlphaBlendFactor = VK_BLEND_FACTOR_ZERO,
+        .dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO,
+        .alphaBlendOp = VK_BLEND_OP_ADD,
+        .colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT |
+                          VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT,
+    };
+
+    const VkPipelineColorBlendStateCreateInfo color_blend_state{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .logicOpEnable = VK_FALSE,
+        .logicOp = VK_LOGIC_OP_COPY,
+        .attachmentCount = 1,
+        .pAttachments = &color_blend_attachment,
+        .blendConstants =
+            {
+                0,
+                0,
+                0,
+                0,
+            },
+    };
+
+    const std::array<VkDynamicState, 2> dynamic_states{
+        VK_DYNAMIC_STATE_VIEWPORT,
+        VK_DYNAMIC_STATE_SCISSOR,
+    };
+
+    const VkPipelineDynamicStateCreateInfo dynamic_state{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .dynamicStateCount = dynamic_states.size(),
+        .pDynamicStates = dynamic_states.data(),
+    };
+
+    const VkGraphicsPipelineCreateInfo pipeline_create_info{
+        .sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .stageCount = static_cast<uint32_t>(shader_stages.size()),
+        .pStages = shader_stages.data(),
+        .pVertexInputState = &vertex_input_state_create_info,
+        .pInputAssemblyState = &input_assembly_state,
+        .pTessellationState = nullptr,
+        .pViewportState = &viewport_state,
+        .pRasterizationState = &rasterization_state,
+        .pMultisampleState = &multisample_state,
+        .pDepthStencilState = &depth_stencil_state,
+        .pColorBlendState = &color_blend_state,
+        .pDynamicState = &dynamic_state,
+        .layout = layout,
+        .renderPass = p_render_pass.get(),
+        .subpass = 0,
+        .basePipelineHandle = VK_NULL_HANDLE,
+        .basePipelineIndex = -1,
+    };
+
+    result =
+        vkCreateGraphicsPipelines(p_device.get(), VK_NULL_HANDLE, 1,
+                                  &pipeline_create_info, nullptr, &pipeline);
+
+    if (result != VK_SUCCESS) {
+        fmt::println("[ERROR]: Failed to create the Vulkan graphics pipeline: ",
+                     result);
+        throw Error::VulkanError;
+    }
+
+    vkDestroyShaderModule(p_device.get(), vertex_shader_module, nullptr);
+    vkDestroyShaderModule(p_device.get(), fragment_shader_module, nullptr);
+}
--- src/graphics.hpp
+++ src/graphics.hpp
@@ -22,8 +22,9 @@ class RenderPass {
 };
 
 struct Framebuffers {
-public:
-    Framebuffers(const Device& device, const Swapchain& swapchain, const RenderPass& render_pass);
+  public:
+    Framebuffers(const Device &device, const Swapchain &swapchain,
+                 const RenderPass &render_pass);
 
     inline VkFramebuffer get(size_t i) const { return framebuffers.at(i); }
 
@@ -35,8 +36,52 @@ public:
         }
     }
 
-private:
+  private:
     const Device &device;
 
     std::vector<VkFramebuffer> framebuffers;
+};
+
+class GraphicsPipeline {
+  public:
+    GraphicsPipeline(
+        const Device &device, const RenderPass &p_render_pass,
+        std::string_view vertex_shader_path,
+        std::string_view fragment_shader_path,
+        std::span<const VkPushConstantRange> push_constant_ranges,
+        std::span<const VkDescriptorSetLayout> descriptor_set_layouts);
+
+    NO_COPY(GraphicsPipeline);
+
+    inline ~GraphicsPipeline() {
+        vkDestroyPipelineLayout(device.get(), layout, nullptr);
+        vkDestroyPipeline(device.get(), pipeline, nullptr);
+    }
+
+  private:
+    VkPipeline pipeline;
+    VkPipelineLayout layout;
+
+    const RenderPass &render_pass;
+
+    const Device &device;
+};
+
+struct Vertex {
+    glm::vec3 position;
+};
+
+constexpr VkVertexInputBindingDescription vertex_input_binding_description{
+    .binding = 0,
+    .stride = sizeof(Vertex),
+    .inputRate = VK_VERTEX_INPUT_RATE_VERTEX,
+};
+
+constexpr static std::array vertex_attribute_descriptions{
+    VkVertexInputAttributeDescription{
+        .location = 0,
+        .binding = 0,
+        .format = VK_FORMAT_R32G32B32_SFLOAT,
+        .offset = offsetof(Vertex, position),
+    },
 };
