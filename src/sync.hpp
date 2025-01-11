530c8e7a9feb498e8f62802de0dddddff7a87ac9
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2025-01-11 15:38:44 -0500

    Formatted document + added utility functions to wait and reset fences.

--- src/sync.hpp
+++ src/sync.hpp
@@ -6,37 +6,37 @@ class Device;
 #include "devices.hpp"
 
 struct Semaphore {
-public:
-    Semaphore(const Device& device);
+  public:
+    Semaphore(const Device &device);
     NO_COPY(Semaphore);
-    
-    inline VkSemaphore get() const {
-        return fence;
-    }
-    
-    inline ~Semaphore() {
-        vkDestroySemaphore(device.get(), fence, nullptr);
-    }
-    
-private:
+
+    inline VkSemaphore get() const { return fence; }
+
+    inline ~Semaphore() { vkDestroySemaphore(device.get(), fence, nullptr); }
+
+  private:
     VkSemaphore fence;
-    const Device& device;
+    const Device &device;
 };
 
 struct Fence {
-public:
-    Fence(const Device& device, bool signaled);
+  public:
+    Fence(const Device &device, bool signaled);
     NO_COPY(Fence);
-    
-    inline VkFence get() const {
-        return fence;
+
+    inline VkFence get() const { return fence; }
+
+    inline void wait() const {
+        VK_ERROR(vkWaitForFences(device.get(), 1, &fence, VK_TRUE, UINT64_MAX));
     }
-    
-    inline ~Fence() {
-        vkDestroyFence(device.get(), fence, nullptr);
+
+    inline void reset() const {
+        VK_ERROR(vkResetFences(device.get(), 1, &fence));
     }
-    
-private:
+
+    inline ~Fence() { vkDestroyFence(device.get(), fence, nullptr); }
+
+  private:
     VkFence fence;
-    const Device& device;
+    const Device &device;
 };
