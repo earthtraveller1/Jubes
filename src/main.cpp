2cc1e91514c80f211184b084e38750d9e0c2be2e
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2025-01-18 21:03:40 -0500

    Rendered a rectangle.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <GLFW/glfw3.h>
 #include <fmt/format.h>
+#include <vulkan/vulkan_core.h>
 
 #include "buffers.hpp"
 #include "devices.hpp"
@@ -9,19 +10,6 @@
 
 constexpr auto WINDOW_WIDTH = 1280;
 constexpr auto WINDOW_HEIGHT = 720;
-
-namespace {
-void recreate_swapchain_and_framebuffers(const Device &device,
-                                         GLFWwindow *window,
-                                         const RenderPass &render_pass,
-                                         Swapchain &swapchain,
-                                         Framebuffers &framebuffers) {
-    framebuffers.destroy();
-    swapchain.destroy();
-    swapchain.create(device, window);
-    framebuffers.create(device, swapchain, render_pass);
-}
-} // namespace
 
 int main() try {
     if (!glfwInit()) {
@@ -59,16 +47,26 @@ int main() try {
     Semaphore image_acquired_semaphore{device};
     Semaphore rendering_done_semaphore{device};
 
-    std::array<Vertex, 3> vertices = {
-        Vertex{{0.0, 0.5, 0.0}},
+    std::array vertices = {
+        Vertex{{0.5, -0.5, 0.0}},
         Vertex{{0.5, 0.5, 0.0}},
         Vertex{{-0.5, 0.5, 0.0}},
+        Vertex{{-0.5, -0.5, 0.0}},
     };
 
-    Buffer buffer{device, vertices.size() * sizeof(vertices[0]),
-                  Buffer::Type::Vertex};
-    buffer.load_using_staging(command_pool, vertices.data(),
-                              vertices.size() * sizeof(vertices[0]));
+    std::array<uint16_t, 6> indices{
+        0, 1, 2, 0, 2, 3,
+    };
+
+    Buffer vertex_buffer{device, vertices.size() * sizeof(vertices[0]),
+                         Buffer::Type::Vertex};
+    vertex_buffer.load_using_staging(command_pool, vertices.data(),
+                                     vertices.size() * sizeof(vertices[0]));
+
+    Buffer index_buffer{device, indices.size() * sizeof(vertices[0]),
+                        Buffer::Type::Index};
+    index_buffer.load_using_staging(command_pool, indices.data(),
+                                    indices.size() * sizeof(indices[0]));
 
     const auto command_buffer = command_pool.allocate_buffer();
 
@@ -77,7 +75,8 @@ int main() try {
         vkWaitForFences(device.get(), 1, &frame_fence_raw, VK_TRUE, UINT64_MAX);
         vkResetFences(device.get(), 1, &frame_fence_raw);
 
-        auto [image_index, should_recreate] = swapchain.acquire_image(image_acquired_semaphore);
+        auto [image_index, should_recreate] =
+            swapchain.acquire_image(image_acquired_semaphore);
         if (should_recreate) {
             vkDeviceWaitIdle(device.get());
             framebuffers.destroy();
@@ -101,13 +100,47 @@ int main() try {
 
         render_pass.begin(command_buffer, swapchain,
                           framebuffers.get(image_index), {1.0, 0.5, 0.5, 1.0});
+
+        vkCmdBindPipeline(command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS,
+                          pipeline.get());
+
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        const VkViewport viewport{
+            .x = 0,
+            .y = 0,
+            .width = static_cast<float>(width),
+            .height = static_cast<float>(height),
+            .minDepth = 0,
+            .maxDepth = 1,
+        };
+        vkCmdSetViewport(command_buffer, 0, 1, &viewport);
+
+        const VkRect2D scissor{.offset =
+                                   {
+                                       .x = 0,
+                                       .y = 0,
+                                   },
+                               .extent = {
+                                   .width = static_cast<uint32_t>(width),
+                                   .height = static_cast<uint32_t>(height),
+                               }};
+        vkCmdSetScissor(command_buffer, 0, 1, &scissor);
+
+        const std::array buffers{vertex_buffer.get()};
+        VkDeviceSize offset = 0;
+        vkCmdBindVertexBuffers(command_buffer, 0, 1, buffers.data(), &offset);
+        vkCmdBindIndexBuffer(command_buffer, index_buffer.get(), offset, VK_INDEX_TYPE_UINT16);
+        vkCmdDrawIndexed(command_buffer, 6, 1, 0, 0, 0);
+
         vkCmdEndRenderPass(command_buffer);
 
         vkEndCommandBuffer(command_buffer);
 
         device.submit_to_graphics(command_buffer, image_acquired_semaphore,
                                   rendering_done_semaphore, frame_fence);
-        should_recreate = device.present(swapchain, rendering_done_semaphore, image_index);
+        should_recreate =
+            device.present(swapchain, rendering_done_semaphore, image_index);
         if (should_recreate) {
             vkDeviceWaitIdle(device.get());
             framebuffers.destroy();
